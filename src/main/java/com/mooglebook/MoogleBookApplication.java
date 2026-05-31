4dac3f8e4787db8e3e9ca9f387d19f68f4178a8a
andrius anselmi <andriusanselmi12@gmail.com>
2026-05-31 11:06:51 -0300

    chore: initial setup

---
+++ src/main/java/com/mooglebook/MoogleBookApplication.java
@@ -0,0 +1,13 @@
+package com.mooglebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoogleBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoogleBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mooglebook/MoogleBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooglebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoogleBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
