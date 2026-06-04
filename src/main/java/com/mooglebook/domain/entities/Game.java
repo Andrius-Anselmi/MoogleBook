46227c1cd7e4479bc7f25d35843aa269b12626cc
andrius anselmi <andriusanselmi12@gmail.com>
2026-06-03 23:45:12 -0300

    feat: add gameSession and review associations to Game record

--- src/main/java/com/mooglebook/domain/entities/Game.java
+++ src/main/java/com/mooglebook/domain/entities/Game.java
@@ -3,11 +3,15 @@ package com.mooglebook.domain.entities;
 import com.mooglebook.domain.enums.Genre;
 import com.mooglebook.domain.enums.Status;
 
+import java.util.List;
+
 public record Game(
         Long id,
         String name,
         Genre genre,
         Status status,
-        String studio
+        String studio,
+        List<GameSession> gameSession,
+        Review review
 ) {
 }
